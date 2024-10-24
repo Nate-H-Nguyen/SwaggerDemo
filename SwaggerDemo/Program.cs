b2a1558d337681351cc7da85a94297ed6ee89bc7
Nate Nguyen <nate.nguyen@silvertech.com>
2024-10-24 12:15:34 -0400

    Add Swagger integration and XML comments for API docs
    Added Swagger services and middleware in `Program.cs` to enable API documentation. Updated `SwaggerDemo.csproj` to generate XML documentation and include `Swashbuckle.AspNetCore` package. Enhanced `WeatherForecast.cs` with XML comments for detailed API documentation.

--- SwaggerDemo/Program.cs
+++ SwaggerDemo/Program.cs
@@ -1,6 +1,18 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "SwaggerDemo", Description = "Swagger Demo", Version = "v1" });
+    var filename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var filePath = Path.Combine(AppContext.BaseDirectory, filename);
+    options.IncludeXmlComments(filePath);
+});
+
+
 
 builder.Services.AddControllers();
 
@@ -11,5 +23,12 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI( options => { 
+options.SwaggerEndpoint("/swagger/v1/swagger.json", "SwaggerDemo v1");
+    options.RoutePrefix = string.Empty;
+    options.DocumentTitle = "Swagger Demo";
+});
 
 app.Run();
--- SwaggerDemo/WeatherForecast.cs
+++ SwaggerDemo/WeatherForecast.cs
@@ -2,12 +2,21 @@ namespace SwaggerDemo
 {
     public class WeatherForecast
     {
+        /// <summary>
+        /// Temperature date
+        /// </summary>
         public DateOnly Date { get; set; }
-
+        /// <summary>
+        /// Temperature in Celsius
+        /// </summary>
         public int TemperatureC { get; set; }
-
+        /// <summary>
+        /// Temperature in Fahrenheit
+        /// </summary>
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+        /// <summary>
+        /// Summary of the weather
+        /// </summary>
         public string? Summary { get; set; }
     }
 }
