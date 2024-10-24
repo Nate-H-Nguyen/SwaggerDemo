aca116a85ab73516d2d56da97587d7fccc71fcfc
Nate Nguyen <naet.nguyen@silvertech.com>
2024-10-24 11:33:07 -0400

    Initial project setup with ASP.NET Core and Swagger
    Added .gitattributes and .gitignore files to manage repository settings and ignore unnecessary files. Created a new Visual Studio solution `SwaggerDemo.sln` and added essential configuration files (`appsettings.json`, `appsettings.Development.json`, `launchSettings.json`). Set up the basic structure of an ASP.NET Core application in `Program.cs` and defined the project in `SwaggerDemo.csproj` with Swagger integration. Added model and controller files (`WeatherForecast.cs`, `HelloWorldController.cs`, `WeatherForecastController.cs`) and an HTTP request file `SwaggerDemo.http` for API testing.

---
+++ SwaggerDemo/Controllers/HellowWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerDemo.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class HellowWorldController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetHelloWorld")]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!!!");
+        }
+    }
+}
